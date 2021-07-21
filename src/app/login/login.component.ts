d3308b2ffdb939ff56f2455ff0f32b818ff4af51
ale <ale@ts.kz>
2021-07-22 00:06:03 +0600

    add methods nca

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,6 +3,12 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { first } from 'rxjs/operators';
 
+declare var signXml: any;
+declare var EventBus: any;
+declare var endConnection: any;
+declare var startConnection: any;
+declare var getActiveTokens: any;
+
 import { AuthenticationService } from '@app/_services';
 
 @Component({ templateUrl: 'login.component.html' })
@@ -14,6 +20,7 @@ export class LoginComponent implements OnInit {
     version: string;
     xml: string;
     error = '';
+    
 
     constructor(
         private formBuilder: FormBuilder,
@@ -33,14 +40,76 @@ export class LoginComponent implements OnInit {
             password: ['', Validators.required]
         });
 
-        // get return url from route parameters or default to '/'
         this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    }
+
+    selectNCAStore() {
+        setTimeout(function () {
+            getActiveTokens("getActiveTokensBack");
+        }, 500);
     }
 
     // convenience getter for easy access to form fields
     // get f() { return this.loginForm.controls; }
 
+    startProcessSign(storage: string) {
+        startConnection();
+        EventBus.subscribe('connect', res => {
+          if (res === 1) {
+    
+            this.signatureDocsConfirm();
+          } else {
+            console.log('Не запущен или не установлен NCALayer', 'Error')
+    
+            EventBus.unsubscribe('connect');
+            EventBus.unsubscribe('token');
+          }
+        });
+      }
+    
+      signatureDocsConfirm() {
+        this.signXmlCall();
+        EventBus.subscribe('signed', async (res) => {
+          console.log('signed start', res);
+          if (res['code'] === '500') {
+            if (res.message !==  'action.canceled') {
+                console.log(`Ошибка NCALayer: ${res.message}`, 'Error')
+            }
+            EventBus.unsubscribe('signed');
+            EventBus.unsubscribe('connect');
+            EventBus.unsubscribe('token');
+            endConnection();
+          }
+    
+          if (res['code'] === '200') {
+            if (res['responseObject'] !== undefined) {
+              const responseObj = res['responseObject'];
+    
+              console.log(responseObj)   // <--- выходной xml
+    
+              endConnection();
+            }
+          }
+        });
+      }
+    
+      signXmlCall() {
+        const xmlToSign = '<xml></xml>'; // <-- генерация xml
+        const selectedStorage = 'PKCS12';
+    
+        signXml(selectedStorage, 'SIGNATURE', xmlToSign, 'signXmlBack');
+      }
+    
+      withOutSpaces(event): boolean {
+        const charCode = event.which ? event.which : event.keyCode;
+        if (charCode === 32) {
+          return false;
+        }
+        return true;
+      }
+
     onSubmit() {
+        
         this.submitted = true;
 
         // // stop here if form is invalid
